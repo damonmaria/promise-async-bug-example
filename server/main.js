2a6c1ae5af131d37b4088d9cea7839243bd59ec3
Damon <damon@mahuika.co.nz>
2016-12-02 23:28:39 +0900

    Comments

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 const sleep = async () =>
   new Promise((resolve) => {
+    // Meteor.setTimeout(resolve, 500)  // makes no difference
     setTimeout(resolve, 500)
   });
 
@@ -9,7 +10,7 @@ const C = new Mongo.Collection('c');
 
 Meteor.startup(async () => {
   console.log('Start of async');
-  await sleep();
+  await sleep();  // Removing this stops the error happening
   console.log('Slept');
   try {
     console.log(C.find().count())
